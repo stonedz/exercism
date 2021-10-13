d32ecb3fd48b80618e9c3eeb0e51936d69202946
Paolo Fagni <paolo.fagni@gmail.com>
2021-10-13 23:02:14 +0200

    SpaceAge

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -9,6 +9,18 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
+earthSecondsYears :: Float
+earthSecondsYears = 31557600
+
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds
-  |  planet == "Earth" = seconds / 31557600
+ageOn planet seconds =
+  case planet of
+    Earth -> earthYears
+    Mercury -> earthYears / 0.2408467
+    Venus -> earthYears / 0.61519726
+    Mars -> earthYears / 1.8808158
+    Jupiter -> earthYears / 11.862615
+    Saturn -> earthYears / 29.447498
+    Uranus -> earthYears / 84.016846
+    Neptune -> earthYears / 164.79132
+    where earthYears = seconds / earthSecondsYears
