47b85259910c50c5ed846bf582a08bee851a873f
Paolo Fagni <paolo.fagni@gmail.com>
2021-10-13 22:12:10 +0200

    Collatz

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,15 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module CollatzConjecture (collatz) where
+import Data.List
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz a
+  | a <= 0 = Nothing
+  | otherwise = Just $ genericLength (generateCollatz a) - 1
+
+
+generateCollatz :: Integer -> [Integer]
+generateCollatz 1 =  [1]
+generateCollatz x
+  | even x = x:generateCollatz (div x 2)
+  | odd x = x:generateCollatz (x * 3 +1)
